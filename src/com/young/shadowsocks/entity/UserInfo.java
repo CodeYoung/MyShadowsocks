f6cfbc8bfe046af2b11837d7b4cb220e88e0c371
Young <237572892@qq.com>
2016-12-13 17:18:14 +0800

    UserInfo

--- src/com/young/shadowsocks/entity/UserInfo.java
+++ src/com/young/shadowsocks/entity/UserInfo.java
@@ -1,12 +1,45 @@
-/**
- * 
- */
 package com.young.shadowsocks.entity;
 
-/**
- * @author Administrator
- *
- */
 public class UserInfo {
+	
+	private int id;
+	private String code;
+	private String name;
+	private String pwd;
 
+	public UserInfo(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
 }
